1d48f935a4beba603085e932715ea19638d9b199
neeboo <neeboo@firestack.one>
2019-07-11 16:18:01 +0800

    fix(testSign):added shardID to latest settings

--- nodejs/testSign.js
+++ nodejs/testSign.js
@@ -7,10 +7,13 @@ const url = 'http://localhost:9500'
 
 // const harmony = new Harmony(url, ChainType.Ethereum, ChainID.Ropsten)
 
-const harmony = new Harmony(url, { chainType: ChainType.Harmony })
+const harmony = new Harmony(url, {
+  chainType: ChainType.Harmony,
+  chainId: ChainID.Default
+})
 
 const testAccs = [
-  '3c8642f7188e05acc4467d9e2aa7fd539e82aa90a5497257cf0ecbb98ed3b88f'
+  '27978f895b11d9c737e1ab1623fde722c04b4f9ccb4ab776bf15932cc72d7c66'
   // 'food response winner warfare indicate visual hundred toilet jealous okay relief tornado'
 ]
 
@@ -23,6 +26,7 @@ const txnObjects = {
   gasPrice: '0',
   gasLimit: '21000',
   shardID: 0,
+  toShardID: 0,
   to: receiver,
   value: '1000000000000000000',
   data: '0x'
@@ -40,37 +44,38 @@ const txnObjects = {
 
 async function main() {
   try {
-    const bbb = await harmony.blockchain.getBalance({ address: sender.address })
+    const bbb = await harmony.blockchain.getBalance({
+      address: sender.address
+    })
     logOutPut('senderBalance', bbb.result)
+    const nonce = await harmony.blockchain.getTransactionCount({
+      address: sender.address
+    })
+    logOutPut('senderNonce', nonce.result)
 
     const tx = harmony.transactions.newTx({
       nonce: txnObjects.nonce,
       gasPrice: new harmony.utils.Unit(txnObjects.gasPrice).asWei().toWei(),
       gasLimit: new harmony.utils.Unit(txnObjects.gasLimit).asWei().toWei(),
       shardID: txnObjects.shardID,
+      toShardID: txnObjects.toShardID,
       to: harmony.crypto.getAddress(txnObjects.to).checksum,
       value: new harmony.utils.Unit(txnObjects.value).asWei().toWei(),
       data: txnObjects.data
     })
-
     const signed = await sender.signTransaction(tx, true)
     logOutPut('Signed Transation', signed.txParams)
-
     const [Transaction, hash] = await signed.sendTransaction()
     logOutPut('Transaction Hash', hash)
-
     // from here on, we use hmy_getTransactionRecept and hmy_blockNumber Rpc api
     // if backend side is not done yet, please delete them from here
-
     const confirmed = await Transaction.confirm(hash)
     logOutPut('Transaction Receipt', confirmed.receipt)
-
     if (confirmed.isConfirmed()) {
       const senderUpdated = await harmony.blockchain.getBalance({
         address: sender.address
       })
       logOutPut('Sender balance', senderUpdated.result)
-
       const receiverUpdated = await harmony.blockchain.getBalance({
         address: receiver
       })
