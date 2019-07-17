7c5208c386c86e1bb85abfc869438b3aa475a7df
Richard Liu <liuzhenglaichn@gmail.com>
2019-07-16 23:29:49 -0700

    multiple call contract in call js

--- tutorial/contracts/example/MyContract.sol
+++ tutorial/contracts/example/MyContract.sol
@@ -2,7 +2,11 @@ pragma solidity ^0.5.1;
 
 
 contract MyContract {
-    function myFunction() public returns(uint256 myNumber, string memory myString) {
+    function print() public returns(uint256 myNumber, string memory myString) {
         return (23456, "Hello!%");
+    }
+
+    function add(uint256 a, uint256 b) public returns(uint256) {
+        return a + b;
     }
 }
