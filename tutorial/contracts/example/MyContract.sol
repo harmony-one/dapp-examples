4a928949144c58aef416853afaa412e313cd7305
neeboo <neeboo@firestack.one>
2019-07-19 16:18:26 +0800

    feat(tutorial):alterContract state and listen events

--- tutorial/contracts/example/MyContract.sol
+++ tutorial/contracts/example/MyContract.sol
@@ -1,7 +1,9 @@
-pragma solidity ^0.5.1;
+pragma solidity ^0.5.2;
 
 
 contract MyContract {
+    uint256 fireNumber;
+
     function print() public pure returns(uint256 myNumber, string memory myString) {
         return (23456, "Hello!%");
     }
@@ -11,7 +13,9 @@ contract MyContract {
     }
 
     event DemoEvent(uint256 num);
-    function fireEvent(uint256 num) public returns(uint256){
+
+    function fireEvent(uint256 num) public returns(uint256) {
+        fireNumber = num;
         emit DemoEvent(num);
         return num;
     }
--- tutorial/src/call.js
+++ tutorial/src/call.js
@@ -13,6 +13,17 @@ export async function callContract(abi, contractAddress, method, ...args) {
     .call({ gasLimit: new BN('210000'), gasPrice: new BN('10000000000') })
   return callResult
 }
+export async function alterContract(abi, contractAddress, method, ...args) {
+  const deployedContract = harmony.contracts.createContract(
+    abi,
+    contractAddress
+  )
+
+  const callResult = await deployedContract.methods[method]
+    .apply(null, args)
+    .send({ gasLimit: new BN('210000'), gasPrice: new BN('10000000000') })
+  return callResult
+}
 
 // command line only
 if (process.argv0 !== undefined && process.argv.slice(2)[0] !== undefined) {
@@ -22,13 +33,50 @@ if (process.argv0 !== undefined && process.argv.slice(2)[0] !== undefined) {
     ? process.argv.slice(2)[1]
     : JSON.parse(fs.readFileSync(process.argv.slice(2)[1], 'utf8'))
         .contractAddress
+
+  const myContract = harmony.contracts.createContract(abi, contractAddress)
+
+  myContract.events
+    .DemoEvent({ fromBlock: 'latest' })
+    .on('data', data => {
+      console.log(data)
+    })
+    .on('changed', changed => {
+      console.log(changed)
+    })
+    .on('error', error => {
+      console.log(error)
+    })
+
   callContract(abi, contractAddress, 'print').then(result => {
     console.log('print', result)
   })
   callContract(abi, contractAddress, 'add', 123, 321).then(result => {
     console.log('add', result)
   })
-  callContract(abi, contractAddress, "fireEvent", 222).then(result => {
-    console.log("fireEvent", result)
+
+  callContract(abi, contractAddress, 'fireEvent', 333).then(() => {
+    console.log('')
+    console.log('----------')
+    console.log('')
+    console.log('called')
+    console.log('')
+    console.log('----------')
+    alterContract(abi, contractAddress, 'fireEvent', 444).then(() => {
+      console.log('')
+      console.log('----------')
+      console.log('')
+      console.log('triggered')
+      console.log('')
+      console.log('----------')
+      callContract(abi, contractAddress, 'fireEvent', 555).then(() => {
+        console.log('')
+        console.log('----------')
+        console.log('')
+        console.log('done')
+        console.log('')
+        console.log('----------')
+      })
+    })
   })
 }
