8ec445c082b36d0740e5f079fb1effd926ace746
neeboo <neeboo@firestack.one>
2019-07-30 21:45:54 +0800

    fix(Index):locales to `en-US`

--- web/Index/config/config.ts
+++ web/Index/config/config.ts
@@ -21,7 +21,7 @@ const plugins: IPlugin[] = [
         // default false
         enable: true,
         // default zh-CN
-        default: 'en',
+        default: 'en-US',
         // default true, when it is true, will use `navigator.language` overwrite default
         baseNavigator: true,
       },
