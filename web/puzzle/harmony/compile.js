6029d3d37cda7e5a68867550d678ba597c5ab3b3
Minh Doan <minh@harmony.one>
2019-09-03 08:32:13 -0700

    add puzzle

---
+++ web/puzzle/harmony/compile.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Puzzle.sol');
+const source = fs.readFileSync(campaignPath, 'utf8');
+const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath);
+
+console.log(output);
+
+for (let contract in output) {
+    fs.outputJsonSync(
+        path.resolve(buildPath, contract.replace(':', '') + '.json'),
+        output[contract]
+    );
+}
