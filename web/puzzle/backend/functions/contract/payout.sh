e58cff54588dbda11a1e2758fda18daf90805edf
Minh Doan <minh@harmony.one>
2019-09-12 23:01:58 -0700

    modified logic for smart contract in shard 1 instead of shard 0

--- web/puzzle/backend/functions/contract/payout.sh
+++ web/puzzle/backend/functions/contract/payout.sh
@@ -1,1 +1,1 @@
-curl -X POST 'http://localhost:5000/newpuzzle-35360/us-central1/payout?address=0x4dcb7672b9bf6955c767fa9170ca29690d54eefd&id=Wds2Jo4Dx5Ze4BeAafWC&level=11&sequence=LRUD'
+curl 'http://localhost:5000/newpuzzle-35360/us-central1/payout?address=0xf48e01968079d7ff6e1ea70bfe71afa1868b7dab&id=ZnyX5nqC9PmnqSIASnRv&level=11&sequence=LRUD'
---
+++ web/puzzle/backend/functions/contract/play.sh
@@ -0,0 +1,1 @@
+curl 'http://localhost:5000/newpuzzle-35360/us-central1/play'
