f05d817ea3458dcf91f3001cee2b4f418db48a73
Minh Doan <minhdoan@Minhs-MacBook-Pro.local>
2019-11-05 12:22:42 -0800

    add comments

--- examples/test2.js
+++ examples/test2.js
@@ -13,6 +13,8 @@ const harmony = new Harmony('http://localhost:9500', {
   chainId: 2,
   chainType: 'hmy'
 });
+
+// TODO: seedphrase created by this.
 const myPhrase = harmony.wallet.newMnemonic();
 const pwd = '1234';
 
@@ -27,7 +29,13 @@ async function decryptThePhrase(keystore, pass) {
 }
 
 async function phraseKeyStore() {
+  // TODO:
+  // create new address and recovery are similar
+  // the only difference is that in creating new address, we have to provide the end user with seed phrase
+  // keyStore for one singe account is the keyStore followings
   const keyStore = await encryptThePhrase(myPhrase, pwd);
+
+  // when signing we need to get private, we can get private key by the followings:
   const recoveredPhrase = await decryptThePhrase(JSON.parse(keyStore), pwd);
   return { myPhrase, keyStore, recoveredPhrase };
 }
